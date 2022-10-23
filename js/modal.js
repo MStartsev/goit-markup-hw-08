89013baa04400e2ba41e3e4a7bf5569a44c72cad
M. A. Startsev <m.a.startsev@gmail.com>
2022-10-23 05:22:37 +0300

    .fix scroll vs js

--- js/modal.js
+++ js/modal.js
@@ -1,22 +1,37 @@
 window.addEventListener('load', () => {
-      const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]")
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    o_hidden: document.querySelector('[o-hidden]'),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-    refs.modal.classList.toggle("not-hidden");
+  let widthScroll = scrollbarWidth();
+
+  function scrollbarWidth() {
+    var documentWidth = parseInt(document.documentElement.clientWidth);
+    var windowsWidth = parseInt(window.innerWidth);
+    var scrollbarWidth = windowsWidth - documentWidth;
+    return scrollbarWidth;
   }
 
-})
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+    refs.modal.classList.toggle('not-hidden');
+    refs.o_hidden.classList.toggle('o-padding');
+    refs.o_hidden.classList.toggle('not-padding');
 
-
-(() => {
-console.log("good")
-  
-})();
+    try {
+      let elem = document.querySelector('.o-padding').style;
+      elem.paddingRight = widthScroll + 'px';
+      document.querySelector('.modal').style.marginLeft = 0;
+    } catch (err) {
+      let elem = document.querySelector('.not-padding').style;
+      elem.paddingRight = 0;
+      document.querySelector('.modal').style.marginLeft = widthScroll / 2 + 'px';
+    }
+  }
+});
