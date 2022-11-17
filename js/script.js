e27440e4671209ad3b889e868b43802b918e87d8
M. A. Startsev <m.a.startsev@gmail.com>
2022-11-17 07:18:18 +0200

    edit portfolio

--- js/script.js
+++ js/script.js
@@ -1,19 +1,19 @@
 (() => {
   respCoef();
-  mobileMenu();
+  events();
 
   window.addEventListener('resize', () => {
     respCoef();
-    mobileMenu();
   });
 
   function respCoef() {
-    let windowInnerWidth = window.innerWidth;
-    let windowClientWidth = document.documentElement.clientWidth;
+    var windowClientWidth = parseInt(document.documentElement.clientWidth);
+    var windowInnerWidth = parseInt(window.innerWidth);
     document.documentElement.style.setProperty('--window-client-width', windowClientWidth);
 
     let coef = windowClientWidth / windowInnerWidth;
     let scrollbar = windowInnerWidth - windowClientWidth;
+    document.documentElement.style.setProperty('--scrollbar-width', scrollbar);
 
     if (windowInnerWidth > 1600) {
       coef = (1600 - scrollbar) / 1200;
@@ -36,71 +36,124 @@
     );
   }
 
-  const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
-    o_hidden: document.querySelector('[data-o-hidden]'),
-  };
+  function events() {
+    const refs = {
+      openModalBtn: document.querySelector('[data-modal-open]'),
+      closeModalBtn: document.querySelector('[data-modal-close]'),
+      modal: document.querySelector('[data-modal]'),
+      o_hidden: document.querySelector('[data-o-hidden]'),
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
-  refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  let widthScroll = scrollbarWidth();
-  document.documentElement.style.setProperty('--scrollbar-width', widthScroll);
-
-  function scrollbarWidth() {
-    var documentWidth = parseInt(document.documentElement.clientWidth);
-    var windowsWidth = parseInt(window.innerWidth);
-    var scrollbarWidth = windowsWidth - documentWidth;
-    return scrollbarWidth;
-  }
-
-  function toggleModal() {
-    refs.modal.classList.toggle('is-hidden');
-    refs.modal.classList.toggle('not-hidden');
-    refs.o_hidden.classList.toggle('o-padding');
-    refs.o_hidden.classList.toggle('not-padding');
-
-    try {
-      let elem = document.querySelector('.o-padding').style;
-      elem.paddingRight = widthScroll + 'px';
-      document.querySelector('.modal').style.marginLeft = 0;
-    } catch (err) {
-      let elem = document.querySelector('.not-padding').style;
-      elem.paddingRight = 0;
-      document.querySelector('.modal').style.marginLeft = widthScroll / 2 + 'px';
-    }
-  }
-
-  //Mobile menu
-  function mobileMenu() {
-    const mobmenu = {
       openMenuBtn: document.querySelector('[data-menu-open]'),
       closeMenuBtn: document.querySelector('[data-menu-close]'),
       contactsMenuBtn: document.querySelector('[data-menu-contacts]'),
       menu: document.querySelector('[data-menu]'),
-      o_hidden: document.querySelector('.js-o-hidden'),
     };
 
-    mobmenu.openMenuBtn.addEventListener('click', toggleMenu);
-    mobmenu.closeMenuBtn.addEventListener('click', toggleMenu);
-    mobmenu.contactsMenuBtn.addEventListener('click', toggleMenu);
+    try {
+      refs.openModalBtn.addEventListener('click', toggleModal);
+      refs.closeModalBtn.addEventListener('click', toggleModal);
+    } catch (error) {
+      console.log('toggleModal not found');
+    }
 
-    function toggleMenu() {
-      mobmenu.menu.classList.toggle('menu__visually-hidden');
-      mobmenu.o_hidden.classList.toggle('o-padding');
-      mobmenu.o_hidden.classList.toggle('not-padding');
+    refs.openMenuBtn.addEventListener('click', toggleMenu);
+    refs.closeMenuBtn.addEventListener('click', toggleMenu);
+    refs.contactsMenuBtn.addEventListener('click', toggleMenu);
+
+    function toggleModal() {
+      refs.modal.classList.toggle('is-hidden');
+      refs.modal.classList.toggle('not-hidden');
+      refs.o_hidden.classList.toggle('o-padding');
+      refs.o_hidden.classList.toggle('not-padding');
+
+      let scrollbar = getComputedStyle(document.documentElement).getPropertyValue(
+        '--scrollbar-width'
+      );
 
       try {
         let elem = document.querySelector('.o-padding').style;
-        elem.paddingRight = widthScroll + 'px';
+        elem.paddingRight = scrollbar + 'px';
         document.querySelector('.modal').style.marginLeft = 0;
       } catch (err) {
         let elem = document.querySelector('.not-padding').style;
         elem.paddingRight = 0;
-        document.querySelector('.modal').style.marginLeft = widthScroll / 2 + 'px';
+        document.querySelector('.modal').style.marginLeft = scrollbar / 2 + 'px';
       }
     }
+
+    function toggleMenu() {
+      refs.menu.classList.toggle('menu__visually-hidden');
+      refs.o_hidden.classList.toggle('o-padding');
+      refs.o_hidden.classList.toggle('not-padding');
+    }
   }
+  //
+  //
+  // const refs = {
+  //   openModalBtn: document.querySelector('[data-modal-open]'),
+  //   closeModalBtn: document.querySelector('[data-modal-close]'),
+  //   modal: document.querySelector('[data-modal]'),
+  //   o_hidden: document.querySelector('[data-o-hidden]'),
+  // };
+
+  // refs.openModalBtn.addEventListener('click', toggleModal);
+  // refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  // let widthScroll = scrollbarWidth();
+  // document.documentElement.style.setProperty('--scrollbar-width', widthScroll);
+
+  // function scrollbarWidth() {
+  //   var documentWidth = parseInt(document.documentElement.clientWidth);
+  //   var windowsWidth = parseInt(window.innerWidth);
+  //   var scrollbarWidth = windowsWidth - documentWidth;
+  //   return scrollbarWidth;
+  // }
+
+  // function toggleModal() {
+  //   refs.modal.classList.toggle('is-hidden');
+  //   refs.modal.classList.toggle('not-hidden');
+  //   refs.o_hidden.classList.toggle('o-padding');
+  //   refs.o_hidden.classList.toggle('not-padding');
+
+  //   try {
+  //     let elem = document.querySelector('.o-padding').style;
+  //     elem.paddingRight = widthScroll + 'px';
+  //     document.querySelector('.modal').style.marginLeft = 0;
+  //   } catch (err) {
+  //     let elem = document.querySelector('.not-padding').style;
+  //     elem.paddingRight = 0;
+  //     document.querySelector('.modal').style.marginLeft = widthScroll / 2 + 'px';
+  //   }
+  // }
+
+  //Mobile menu
+  // function mobileMenu() {
+  //   const mobmenu = {
+  //     openMenuBtn: document.querySelector('[data-menu-open]'),
+  //     closeMenuBtn: document.querySelector('[data-menu-close]'),
+  //     contactsMenuBtn: document.querySelector('[data-menu-contacts]'),
+  //     menu: document.querySelector('[data-menu]'),
+  //     o_hidden: document.querySelector('.js-o-hidden'),
+  //   };
+
+  //   mobmenu.openMenuBtn.addEventListener('click', toggleMenu);
+  //   mobmenu.closeMenuBtn.addEventListener('click', toggleMenu);
+  //   mobmenu.contactsMenuBtn.addEventListener('click', toggleMenu);
+
+  //   function toggleMenu() {
+  //     mobmenu.menu.classList.toggle('menu__visually-hidden');
+  //     mobmenu.o_hidden.classList.toggle('o-padding');
+  //     mobmenu.o_hidden.classList.toggle('not-padding');
+
+  //     try {
+  //       let elem = document.querySelector('.o-padding').style;
+  //       elem.paddingRight = widthScroll + 'px';
+  //       document.querySelector('.modal').style.marginLeft = 0;
+  //     } catch (err) {
+  //       let elem = document.querySelector('.not-padding').style;
+  //       elem.paddingRight = 0;
+  //       document.querySelector('.modal').style.marginLeft = widthScroll / 2 + 'px';
+  //     }
+  //   }
+  // }
 })();
