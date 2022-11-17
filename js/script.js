a5b0f2be0eb663c20334477399fcb59f61d50ae5
M. A. Startsev <m.a.startsev@gmail.com>
2022-11-17 10:50:16 +0200

    resp-adaptiv version final

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+// © M. Startsev - 2022, https://github.com/MStartsev
+
 (() => {
   respCoef();
   events();
@@ -87,73 +89,4 @@
       refs.o_hidden.classList.toggle('not-padding');
     }
   }
-  //
-  //
-  // const refs = {
-  //   openModalBtn: document.querySelector('[data-modal-open]'),
-  //   closeModalBtn: document.querySelector('[data-modal-close]'),
-  //   modal: document.querySelector('[data-modal]'),
-  //   o_hidden: document.querySelector('[data-o-hidden]'),
-  // };
-
-  // refs.openModalBtn.addEventListener('click', toggleModal);
-  // refs.closeModalBtn.addEventListener('click', toggleModal);
-
-  // let widthScroll = scrollbarWidth();
-  // document.documentElement.style.setProperty('--scrollbar-width', widthScroll);
-
-  // function scrollbarWidth() {
-  //   var documentWidth = parseInt(document.documentElement.clientWidth);
-  //   var windowsWidth = parseInt(window.innerWidth);
-  //   var scrollbarWidth = windowsWidth - documentWidth;
-  //   return scrollbarWidth;
-  // }
-
-  // function toggleModal() {
-  //   refs.modal.classList.toggle('is-hidden');
-  //   refs.modal.classList.toggle('not-hidden');
-  //   refs.o_hidden.classList.toggle('o-padding');
-  //   refs.o_hidden.classList.toggle('not-padding');
-
-  //   try {
-  //     let elem = document.querySelector('.o-padding').style;
-  //     elem.paddingRight = widthScroll + 'px';
-  //     document.querySelector('.modal').style.marginLeft = 0;
-  //   } catch (err) {
-  //     let elem = document.querySelector('.not-padding').style;
-  //     elem.paddingRight = 0;
-  //     document.querySelector('.modal').style.marginLeft = widthScroll / 2 + 'px';
-  //   }
-  // }
-
-  //Mobile menu
-  // function mobileMenu() {
-  //   const mobmenu = {
-  //     openMenuBtn: document.querySelector('[data-menu-open]'),
-  //     closeMenuBtn: document.querySelector('[data-menu-close]'),
-  //     contactsMenuBtn: document.querySelector('[data-menu-contacts]'),
-  //     menu: document.querySelector('[data-menu]'),
-  //     o_hidden: document.querySelector('.js-o-hidden'),
-  //   };
-
-  //   mobmenu.openMenuBtn.addEventListener('click', toggleMenu);
-  //   mobmenu.closeMenuBtn.addEventListener('click', toggleMenu);
-  //   mobmenu.contactsMenuBtn.addEventListener('click', toggleMenu);
-
-  //   function toggleMenu() {
-  //     mobmenu.menu.classList.toggle('menu__visually-hidden');
-  //     mobmenu.o_hidden.classList.toggle('o-padding');
-  //     mobmenu.o_hidden.classList.toggle('not-padding');
-
-  //     try {
-  //       let elem = document.querySelector('.o-padding').style;
-  //       elem.paddingRight = widthScroll + 'px';
-  //       document.querySelector('.modal').style.marginLeft = 0;
-  //     } catch (err) {
-  //       let elem = document.querySelector('.not-padding').style;
-  //       elem.paddingRight = 0;
-  //       document.querySelector('.modal').style.marginLeft = widthScroll / 2 + 'px';
-  //     }
-  //   }
-  // }
 })();
